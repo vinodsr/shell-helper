dd278915ce47284d9ac414ebcbd00daa87d196cd
Vinod S R <smartvinu@gmail.com>
2020-04-26 13:49:31 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# shell-helper
+My Shell Scripts
---
+++ functions.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+dev() {
+    IFS='/'
+    ARG_PATH="$*"
+    FULL_PATH=~/dev/$ARG_PATH
+    mkdir -p $FULL_PATH
+    cd $FULL_PATH
+    ls -ltr
+    echo "enjoy! \U1F920"
+}
+
+cdx() {
+    FULL_PATH=$1
+    cd $FULL_PATH 2>/dev/null
+    if [ $? -ne 0 ]; then
+        mkdir $FULL_PATH 2>/dev/null
+        echo "\U1F69C : ${FULL_PATH}"
+        cd $FULL_PATH
+    else
+        ls -ltr
+    fi
+}
